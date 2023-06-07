ba7361c11a9061f0a844952d1e68e27aa4301599
GuillaumeChk <guillaume.chekhab39@gmail.com>
2023-06-07 13:21:32 +0200

    add images on homepage

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,29 @@
 <template>
   <q-page class="flex flex-center">
-    <img
+    <!-- <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
+    /> -->
+
+    <q-img
+      src="~assets/images/Jura_neige_pâture.jpg"
+      style="width: 200px; height: 200px"
     >
+    </q-img>
+    <q-img
+      loading="lazy"
+      src="~assets/images/Jura_en_hiver.jpg"
+      style="width: 200px; height: 200px"
+    >
+    </q-img>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+});
 </script>
